28ffdddcb49668285e12cc4e95efc609b48bec98
Racquel Munoz <racquelg85@hotmail.com>
2022-10-11 15:01:24 -0500

    added log out/log in

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
-const exphbs = require('express-handlebars');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,11 +9,9 @@ const PORT = process.env.PORT || 3001;
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const hbs = exphbs.create({});
-
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {}, 
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
@@ -21,11 +19,13 @@ const sess = {
   })
 };
 
+app.use(session(sess));
+
+const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
